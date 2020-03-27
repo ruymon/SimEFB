7c693bb916d10bf4db83ec57ce5748f84d9a317e
Ruy Monteiro <ruystfrancis@gmail.com>
2020-03-26 22:28:32 -0300

    v0.0.7
    Hotfix 2

--- app/include/head.php
+++ app/include/head.php
@@ -13,9 +13,13 @@
     <title>SimEFB - dev.</title>
 
     <!-- Vendor css -->
+    <link rel="shortcut icon" href="/simefb/img/simefb_logo.png" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <link href="../lib/font-awesome/css/font-awesome.css" rel="stylesheet">
     <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
     <link href="../lib/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
     <!-- Style CSS -->
     <link rel="stylesheet" href="../../css/style.css">
