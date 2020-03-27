0e2d57a8f1b015d9e6861b24ee431efaf3aa4506
Ruy Monteiro <ruystfrancis@gmail.com>
2020-03-26 23:02:10 -0300

    v0.0.7
    Hotfix 3
     Modal

--- app/include/head.php
+++ app/include/head.php
@@ -2,7 +2,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    
     <link rel="shortcut icon" href="/simefb/img/simefb_logo.png" />
     
 
@@ -14,12 +14,12 @@
 
     <!-- Vendor css -->
     <link rel="shortcut icon" href="/simefb/img/simefb_logo.png" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <link href="../lib/font-awesome/css/font-awesome.css" rel="stylesheet">
     <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
     <link href="../lib/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet">
+    <link href="../lib/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    
 
     <!-- Style CSS -->
     <link rel="stylesheet" href="../../css/style.css">
